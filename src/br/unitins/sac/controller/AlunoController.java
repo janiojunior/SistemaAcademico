9efbf66fcf1fdea7e25ffa2b3f40ff5cdb929060
janio <janiojunior@gmail.com>
2017-08-22 16:21:31 -0300

    Congig persistence

--- src/br/unitins/sac/controller/AlunoController.java
+++ src/br/unitins/sac/controller/AlunoController.java
@@ -1,10 +1,17 @@
 package br.unitins.sac.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Query;
+
+import br.unitins.sac.factory.JPAFactory;
 import br.unitins.sac.model.Aluno;
 
 public class AlunoController extends Controller<Aluno>{
 
 	private Aluno entity;
+	private List<Aluno> listaAluno;
 	
 	@Override
 	public Aluno getEntity() {
@@ -14,7 +21,22 @@ public class AlunoController extends Controller<Aluno>{
 	}
 	
 	
-	
+	public List<Aluno> getListaAluno() {
+		if (listaAluno == null) {
+			em = JPAFactory.getEntityManager();
+			Query query = em.createQuery("Select s From Servidor s Order by s.id Desc");
+			
+			listaAluno = query.getResultList();
+			
+			if (listaAluno == null)
+				listaAluno = new ArrayList<Aluno>();
+		}
+		return listaAluno;
+	}
+
+	public void setListaAluno(List<Aluno> listaServidor) {
+		this.listaAluno = listaServidor;
+	}
 	
 
 }
